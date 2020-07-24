52a8b66cfacdb9a2558f782929c46a7919554e82
jsingh-git <jsingh.bh@outlook.com.au>
2020-07-24 22:07:35 +1000

    updated main

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ export default class Main extends Component {
     render() {
         return (
             <div className="container">
-                <h1 className="title">React Movie Search</h1>
+                <h1 className="title">Movie Search</h1>
                 <p className="p-main">This app uses the TMDb API but is not endorsed or certified by:<a href="https://www.themoviedb.org/">TMDb</a></p>
                 
                 <SearchMovies />
