a98a93c5e484b760a71e2877d224550df4cc182f
jsingh-git <jsingh.bh@outlook.com.au>
2020-07-24 21:49:21 +1000

    TMDB reference
    added TMDB reference main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,8 @@ export default class Main extends Component {
         return (
             <div className="container">
                 <h1 className="title">React Movie Search</h1>
+                <p className="p-main">This app uses the TMDb API but is not endorsed or certified by:<a href="https://www.themoviedb.org/">TMDb</a></p>
+                
                 <SearchMovies />
             </div>
         );
