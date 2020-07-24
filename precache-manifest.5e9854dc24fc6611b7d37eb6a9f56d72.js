4925891018a548193484fce6b8d5efc6edecbd76
jsingh-git <jsingh.bh@outlook.com.au>
2020-07-24 21:11:20 +1000

    Updates

---
+++ precache-manifest.5e9854dc24fc6611b7d37eb6a9f56d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31ad71d6b62f0b5f8f92d7d555a2be4",
+    "url": "/searchmovie/index.html"
+  },
+  {
+    "revision": "43ec90e7c61f9a9249c3",
+    "url": "/searchmovie/static/css/main.1e0319c5.chunk.css"
+  },
+  {
+    "revision": "415757354781e7390fa3",
+    "url": "/searchmovie/static/js/2.13153d0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/searchmovie/static/js/2.13153d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ec90e7c61f9a9249c3",
+    "url": "/searchmovie/static/js/main.dbd3fd25.chunk.js"
+  },
+  {
+    "revision": "926caf9e4d8bae441817",
+    "url": "/searchmovie/static/js/runtime-main.7a2489ea.js"
+  }
+]);
